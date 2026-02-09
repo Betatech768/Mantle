af636321559349b5225448d033ec38603e4644e6
Goodness  Onwuanaku <gudzy86@gmail.com>
2026-02-09 11:59:23 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,13 +1,34 @@
 import sys
 
 
+BUILTINS = {
+    "exit" : cmd_exit,
+    "echo" : lambda *args: print(" ".join(args)),
+    "help" : lambda: print("available commands exit, help, echo, clear"),
+    "clear": lambda: print("\033c", end= "")
+    if sys.platform ="win32"
+    else os.system('cls'),
+}
+
 def main():
     # TODO: Uncomment the code below to pass the first stage
    while True:
-    command = input("$ ").lower()
-    if command == "exit":
-        break 
-    print(f"{command}: command not found")
+    try:
+        command = input('$ ').strip()
+        if not command:
+            continue 
+        parts = shlex.split(command)
+        userCommand = parts[0]
+        args = parts[1:]
+
+        if userCommand in BUILTINS:
+            BUILTINS[userCommand](*args)
+        else:
+            print(f"{userCommand}: command not found")
+        except KeyboardInterrupt:
+            print()
+        except Exception as e:
+            print(f"{userCommand} not found, error - {e}")
 
 
 
