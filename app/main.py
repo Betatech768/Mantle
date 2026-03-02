8016e9c76cfda6cf441793058691932ebc0a9e7d
Goodness  Onwuanaku <gudzy86@gmail.com>
2026-03-02 21:21:25 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -124,9 +124,7 @@ def get_executable_name():
             for filename in os.listdir(directory):
                 full_path = os.path.join(directory, filename)
                 
-                if os.path.isfile(full_path) and ext in PATHEXT:
-                    executable.add(filename)
-                elif os.path.isfile(full_path):
+                if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
                     executable.add(filename)
         except(FileNotFoundError, PermissionError, OSError):
             continue
